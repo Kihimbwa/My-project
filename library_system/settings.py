667d2e2c3dd15810400122507049bef1401ee69f
Kihimbwa <nurudinikihimbwa@gmail.com>
2026-03-03 00:06:14 +0300

    Fix CORS for Vercel frontend

--- library_system/settings.py
+++ library_system/settings.py
@@ -23,9 +23,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # muhimu kwa static
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
